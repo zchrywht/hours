3dd98f4a11fabd753edf350e29f9f9939d457d6f
Zachary White <zachary.r.white@gmail.com>
2022-02-28 19:16:08 -0500

    test threading on raspberry pi

--- test/app.py
+++ test/app.py
@@ -1,36 +1,42 @@
-#from flask import Flask, render_template, request, redirect
-from quart import Quart, render_template, request, redirect
+'''
+# call these to stop the background thread:
+loop.call_soon_threadsafe(loop.stop)
+loop.close()
+'''
+
 import asyncio
 from bleak import BleakClient, BleakScanner
-import nest_asyncio
-
-#nest_asyncio.apply()
+import time
+from threading import Thread
+from flask import Flask, render_template, request, redirect
 
 TIMER_NAME = "GxTimer_31A0"
-IO_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 
-COMMANDS = {
-    "power" : bytes.fromhex("1F30 4145 09"),
-    "reset" : bytes.fromhex("1F30 414F 09"),
-    "start" : bytes.fromhex("1F30 4151 09"),
-    "stop" : bytes.fromhex("1F30 4150 09"),
-    "edit" : bytes.fromhex("1F30 4146 09"),
-    "left" : bytes.fromhex("1F30 4148 09"),
-    "right" : bytes.fromhex("1F30 4149 09"),
-    "enter" : bytes.fromhex("1F30 4152 09"),
-    "0" : bytes.fromhex("1F30 4130 09"),
-    "1" : bytes.fromhex("1F30 4131 09"),
-    "2" : bytes.fromhex("1F30 4132 09"),
-    "3" : bytes.fromhex("1F30 4133 09"),
-    "4" : bytes.fromhex("1F30 4134 09"),
-    "5" : bytes.fromhex("1F30 4135 09"),
-    "6" : bytes.fromhex("1F30 4136 09"),
-    "7" : bytes.fromhex("1F30 4137 09"),
-    "8" : bytes.fromhex("1F30 4138 09"),
-    "9" : bytes.fromhex("1F30 4139 09"),
-}
+async def sendCommand(command):
 
-async def sendCommand(client, command):
+    IO_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+
+    COMMANDS = {
+        "power" : bytes.fromhex("1F30 4145 09"),
+        "reset" : bytes.fromhex("1F30 414F 09"),
+        "start" : bytes.fromhex("1F30 4151 09"),
+        "stop" : bytes.fromhex("1F30 4150 09"),
+        "edit" : bytes.fromhex("1F30 4146 09"),
+        "left" : bytes.fromhex("1F30 4148 09"),
+        "right" : bytes.fromhex("1F30 4149 09"),
+        "enter" : bytes.fromhex("1F30 4152 09"),
+        "0" : bytes.fromhex("1F30 4130 09"),
+        "1" : bytes.fromhex("1F30 4131 09"),
+        "2" : bytes.fromhex("1F30 4132 09"),
+        "3" : bytes.fromhex("1F30 4133 09"),
+        "4" : bytes.fromhex("1F30 4134 09"),
+        "5" : bytes.fromhex("1F30 4135 09"),
+        "6" : bytes.fromhex("1F30 4136 09"),
+        "7" : bytes.fromhex("1F30 4137 09"),
+        "8" : bytes.fromhex("1F30 4138 09"),
+        "9" : bytes.fromhex("1F30 4139 09"),
+    }
+
     await client.write_gatt_char(IO_UUID, COMMANDS[command])
     return
 
@@ -45,34 +51,112 @@ async def discover():
                     print("Timer Found")
                     return d.address
 
-async def main():
-    address = await discover()
+async def pair():
+    client =  BleakClient(address)
     while True:
         try:
-            async with BleakClient(address) as client:
-
-                await client.pair()
-
-                app = Quart(__name__)
-
-                @app.route('/')
-                def index():
-                    return render_template('index.html')
-
-                @app.route('/power', methods=['POST'])
-                def power():
-                    #loop = asyncio.get_event_loop()
-                    #print(loop)
-                    #asyncio.run_coroutine_threadsafe(sendCommand(client, "power"), loop)
-                    #await client.write_gatt_char(IO_UUID, COMMANDS['power'])
-                    return redirect('/')
-
-                if __name__ == '__main__':
-                    await app.run_task(debug=False, host='0.0.0.0')
-                    #app.run(debug=False, host='0.0.0.0') # nothing after this will execute
-
+            if not client.is_connected:
+                print("connecting...")
+                await client.connect()
+                print("connected")
+            print("pairing...")
+            await client.pair()
+            print("paired.")
+            return client
         except Exception as e:
-            print(e)
+            print("pairing failed, retrying...")
+            pass
 
+def stop():
+    disconnected = asyncio.run_coroutine_threadsafe(client.disconnect(), loop).result()
+    loop.call_soon_threadsafe(loop.stop)
+    while True:
+        if not loop.is_running():
+            loop.close()
+            return
 
-asyncio.run(main())
+def start_background_loop(loop: asyncio.AbstractEventLoop) -> None:
+    asyncio.set_event_loop(loop)
+    loop.run_forever()
+
+def start() -> None:
+    # create new event loop, assign to "background" thread, start
+    global loop
+    loop = asyncio.new_event_loop()
+    t = Thread(target=start_background_loop, args=(loop,))
+    t.start()
+
+    # find timer and pair with it
+    global address
+    address_future = asyncio.run_coroutine_threadsafe(discover(), loop)
+    address = address_future.result()
+
+    global client
+    client_future = asyncio.run_coroutine_threadsafe(pair(), loop)
+    client = client_future.result()
+
+# commands
+
+def stringToSeconds(t):
+    # takes a time in format "HHMMSS" and converts it into seconds
+    hours = int(t[:2])
+    minutes = int(t[2:4])
+    seconds = int(t[4:])
+
+    return ((hours * 3600) + (minutes * 60) + seconds)
+
+def threadCommand(command) -> None:
+    asyncio.run_coroutine_threadsafe(sendCommand(command), loop)
+
+def setTime(t):
+    if len(t) == 6 and t.isnumeric():
+        threadCommand("stop")
+        threadCommand("reset")
+        threadCommand("reset")
+        threadCommand("edit")
+        for i in range(len(t)):
+            threadCommand(t[i])
+        threadCommand("enter")
+        return True
+    else:
+        return False
+
+def runReset(startTime, maxTime):
+    setTime(startTime)
+    time.sleep(0.5)
+    threadCommand("start")
+    now = time.time()
+    cycleLengthSeconds = stringToSeconds(maxTime)
+    startTimeSeconds = stringToSeconds(startTime)
+    endTime = now + cycleLengthSeconds - startTimeSeconds
+    while True:
+        time.sleep(0.1)
+        now = time.time()
+        if now > endTime:
+            threadCommand("stop")
+            time.sleep(0.1)
+            threadCommand("reset")
+            time.sleep(0.1)
+            threadCommand("reset")
+            return
+
+def cycle(startTime, maxTime):
+    while True:
+        runReset(startTime, maxTime)
+
+# define app
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/power', methods=['POST'])
+def power():
+    threadCommand("power")
+    return redirect('/')
+
+if __name__ == "__main__":
+    start()
+    app.run(debug=False, host='0.0.0.0') # nothing after this will execute
+    stop()
