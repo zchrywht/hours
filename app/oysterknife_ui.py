5ecd8b0196bd5b31729a80b5c079fdd1e00820b1
zchrywht <zachary.r.white@gmail.com>
2024-12-08 16:51:26 -0500

    dist version with requirements.txt

--- app/oysterknife_ui.py
+++ app/oysterknife_ui.py
@@ -1,8 +1,6 @@
-import time
 from datetime import datetime, timedelta
 import board
 import digitalio
-import serial
 import adafruit_character_lcd.character_lcd as characterlcd
 from analog import getInput
 
